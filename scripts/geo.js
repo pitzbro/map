47a2ee993669f537f0e2b0beff4865e8100cfb79
pitzbro <asioren@gmail.com>
2017-04-05 19:00:34 +0300

    first commit

---
+++ scripts/geo.js
@@ -0,0 +1,120 @@
+var geoPoints = [
+    {
+        x: 100,
+        y: 100,
+        lat: 31.046051,
+        lon: 34.851612,
+        id: 1,
+        originalId: 1,
+        name: 'A',
+        filtered: true,
+        marker: true,
+        productType: "CCE",
+        adminState: "UNLOCKED",
+        operState: "AVAILABLE"
+    },
+    {
+        x: 500,
+        y: 100,
+        lat: 40.712784,
+        lon: -74.005941,
+        id: 2,
+        name: 'B',
+        filtered: true,
+        marker: true,
+        originalId: 2,
+        productType: "GENERIC",
+        adminState: "LOCKED",
+        operState: "AVAILABLE"
+    },
+    {
+        x: 300,
+        y: 300,
+        id: 3,
+        lat: 52.355518,
+        lon: -1.174320,
+        originalId: 3,
+        name: 'C',
+        filtered: false,
+        marker: false,
+        productType: "CLOUDBOND",
+        adminState: "UNLOCKED",
+        operState: "AVAILABLE"
+    },
+    {
+        x: 200,
+        y: 500,
+        lat: 35.126413,
+        lon: 33.429859,
+        id: 4,
+        originalId: 4,
+        name: 'D',
+        filtered: false,
+        marker: false,
+        productType: "test_unknown_type",
+        adminState: "LOCKED",
+        operState: "AVAILABLE"
+    }
+]
+
+var geoLinks = [
+    {
+        id: 1,
+        name: 'A-B',
+        adminState: 'LOCKED',
+        filtered: true,
+        marker: true,
+        connectionType: 'connection',
+        originalId: 1,
+        from: 1,
+        to: 2
+    },
+    {
+        id: 2,
+        name: 'A-D',
+        adminState: 'UNLOCKED',
+        filtered: true,
+        marker: true,
+        connectionType: 'connection',
+        originalId: 2,
+        direction: 'IN',
+        from: 1,
+        to: 3
+    },
+    {
+        id: 3,
+        name: 'B-E',
+        adminState: 'LOCKED',
+        filtered: false,
+        marker: false,
+        connectionType: 'connection',
+        originalId: 3,
+        direction: 'OUT',
+        from: 1,
+        to: 4
+    },
+    {
+        id: 4,
+        name: 'D-E',
+        adminState: 'UNLOCKED',
+        filtered: false,
+        marker: false,
+        connectionType: 'connection',
+        originalId: 4,
+        direction: 'BOTH',
+        from: 4,
+        to: 2
+    },
+    {
+        id: 5,
+        name: 'C-A',
+        adminState: 'LOCKED',
+        filtered: true,
+        marker: true,
+        connectionType: 'connection',
+        direction: 'OUT',
+        originalId: 5,
+        from: 2,
+        to: 1
+    }
+]
